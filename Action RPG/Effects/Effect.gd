2a8d3f3cb8d34c83709719f25e41a5aad1120fc4
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2020-05-27 02:47:57 -0300

    Enemy AI Começando

--- Action RPG/Effects/Effect.gd
+++ Action RPG/Effects/Effect.gd
@@ -3,7 +3,7 @@ extends AnimatedSprite
 func _ready():
 	connect("animation_finished", self, "_on_animation_finished")
 	frame = 0
-	play("Animate")
+	play("Animated")
 
 
 func _on_animation_finished():
